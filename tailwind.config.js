8cc43c911bc2c078f7b3587b5dd434130f6d172b
Emmanuelomolaju <emmanuelomolaju964@gmail.com>
2025-06-11 09:20:02 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+        serif: ["Instrument Serif", "serif"],
+      },
+      colors: {
+        midnight: "#0C2A44",
+        gold: "#AEA797",
+        cream: "#F1EAE0",
+        dark: "#2A2927",
+        navy: "#0C2A44",
+      },
+    },
+  },
+  plugins: [],
+};
