0c22e8adbf772ea9b1b35c6ef2c642dee72a471e
Emmanuelomolaju <emmanuelomolaju964@gmail.com>
2025-06-13 09:17:08 +0100

    add: about, blog section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./about-us/*.{html,js}", "./**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "!./node_modules/**/*"],
   theme: {
     extend: {
       fontFamily: {
